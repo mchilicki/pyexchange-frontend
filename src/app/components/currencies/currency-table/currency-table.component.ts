91b41aa2cd81deaf77e88c8e45e955df2b6d86c6
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2020-01-20 21:11:29 +0100

    Add buy option to all currencies table

--- src/app/components/currencies/currency-table/currency-table.component.ts
+++ src/app/components/currencies/currency-table/currency-table.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CurrencyListItem } from 'src/app/models/currency/currency-list-item';
 import { defaults } from 'src/app/common/defaults';
+import { MatDialog } from '@angular/material/dialog';
+import { CurrencyDialogComponent } from '../../dialogs/currency-dialog/currency-dialog.component';
+import { OperationType } from 'src/app/common/operation-type';
+import { Currency } from 'src/app/models/currency/currency';
+import { CurrencyService } from 'src/app/services/currency.service';
+import { CurrencyAmount } from 'src/app/models/currency/currency-amount';
+import { SnackbarService as SnackBarService } from 'src/app/services/infrastructure/snack-bar.service';
 
 @Component({
   selector: 'app-currency-table',
@@ -12,8 +19,28 @@ export class CurrencyTableComponent implements OnInit {
   columnsToDisplay = ['name', 'code', 'unit', 'purchase_price', 'actions'];
   defaults = defaults;
 
-  constructor() { }
+  constructor(private currencyService: CurrencyService,
+              private snackBarService: SnackBarService,
+              private dialog: MatDialog) { }
 
   ngOnInit() { }
 
+  openBuyCurrencyDialog(currency: CurrencyListItem) {
+    const dialogRef = this.dialog.open(CurrencyDialogComponent, {
+      data: {
+        operationType: OperationType.BUY,
+        currency: currency as Currency
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.currencyService.buyCurrency(currency.id, result as CurrencyAmount).subscribe(user => {
+          this.snackBarService.openSnackBar(`You've bought ${result.amount} ${currency.code}`);
+        }, error => {
+          this.snackBarService.openSnackBar(error.error.error, 'Dismiss', true);
+        });
+      }
+    });
+  }
 }
